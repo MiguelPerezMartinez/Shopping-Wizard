63998b2404ec762d01b9ce077ec009fe65dcc62d
Miguel Perez Martinez <migueldw2.0@gmail.com>
2021-03-23 21:42:23 +0100

    recibir y mostrar info de cliente. cambio de botones

--- web/info.js
+++ web/info.js
@@ -1,3 +1,21 @@
-// import client from './client';
+$profile_list = document.querySelector("#profile-info");
+$address_list = document.querySelector("#address-info");
+$inputs = document.querySelectorAll(".validable");
+$inputs.forEach((input) => {
+  input.setAttribute("autocomplete","off");
+  input.addEventListener('focusout',printInfo);
+});
 
-// console.log(client.username);
+function printInfo(){
+  var li = document.createElement("li");
+  $profile_list.innerHTML = '<li>Username: ' + client.username + '</li>';
+  $profile_list.innerHTML += '<li>Email: ' + client.email + '</li>';
+
+  $address_list.innerHTML = '<li>First name: ' + client.firstName + '</li>';
+  $address_list.innerHTML += '<li>Last name: ' + client.lastName + '</li>';
+  $address_list.innerHTML += '<li>Birthday: ' + client.birthday + '</li>';
+  $address_list.innerHTML += '<li>Address: ' + client.address1 + ' ' + client.address2 + '</li>';
+  $address_list.innerHTML += '<li>Postal code: ' + client.postalCode + '</li>';
+  $address_list.innerHTML += '<li>Country: ' + client.country + '</li>';
+  $address_list.innerHTML += '<li>Phone: ' + client.phoneCode + ' ' + client.phone + '</li>';
+}
